337d3ae6d466fa5e3052d093d03a6a129d58275f
id2k1149 <id2k1149@gmail.com>
2022-10-10 19:45:20 -0400

    Edit ViewController

--- MyDaysApp/ViewController.swift
+++ MyDaysApp/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var infoLabel: UILabel!
+    private var numberOfDays = ""
+    
+//    @IBOutlet weak var resultButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Change corner radius
+//        resultButton.layer.cornerRadius = 12
     }
 
-
+    @IBAction func datePicker(_ sender: UIDatePicker) {
+        let range = sender.date..<Date.now
+        numberOfDays = range.formatted(.components(style: .wide,
+                                                   fields: [.day]))
+    }
+    @IBAction func resultButtonTapped() {
+        infoLabel.text = "You've been enjoying your life for \(numberOfDays)"
+    }
+    
 }
 
