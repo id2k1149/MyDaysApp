b71b7e799ec78b7c7b7f62e20b77ecde9928805c
id2k1149 <id2k1149@gmail.com>
2022-10-10 18:31:09 -0400

    Initial Commit

---
+++ MyDaysApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyDaysApp
+//
+//  Created by Max Franz Immelmann on 10/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
